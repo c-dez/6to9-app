adb97e477bdb49fe753e729676dec44547f46ded
c-dez <hdezcruzcarlos@gmail.com>
2023-07-18 22:37:40 -0600

    submit btn creates new div in div .list w array value

--- main.js
+++ main.js
@@ -2,11 +2,26 @@ const taskList = []
 
 const submit = document.querySelector('#submit')
 
-const addToList = ()=>{
+const addToArray = ()=>{
     const inputTask = document.querySelector('#inputTask')
-    taskList.push(inputTask.value)
-    console.log(taskList)
-    inputTask.value = ''
+    if(inputTask.value){
+
+        taskList.push(inputTask.value)
+        console.log(taskList)
+        createList()
+        inputTask.value = ''
+    }
 }
 
-submit.addEventListener('click',addToList)
+
+const createList = ()=>{
+    const list = document.querySelector('.list')
+   
+        let task = document.createElement('div')
+        task.textContent = taskList[taskList.length -1]
+        list.appendChild(task)
+    
+    
+}
+submit.addEventListener('click',addToArray)
+
